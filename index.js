81b72a40735e7d6bbea72101c403637d43016021
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-06 15:56:06 -0500

    commit tree 3cf29a7a28a7d1b9d63a508b1daa9d5d8c17233a

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){customerName = customerName.toUpperCase(); return customerName}
+function setBestCustomer(){bestCustomer = 'not bob'}
+function overwriteBestCustomer(){bestCustomer = 'maybe bob'}
+const leastFavoriteCustomer = 'yoyo'
+function changeLeastFavoriteCustomer() { leastFavoriteCustomer = 'nono'}
